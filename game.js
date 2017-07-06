ca1ee59d21828f9e921a1dd7e1d91c620d1cb4ae
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-06 19:26:05 +0530

    started frontend

---
+++ game.js
@@ -0,0 +1,83 @@
+(function(){
+
+	var app = angular.module('game', [ ]);
+	
+	var row = 0;
+	var column = 0;
+
+	app.controller('GameController', function($scope){
+		$scope.board = createBoard();
+		$scope.score = 0;
+		$scope.go = function(spot){
+			var y=check(spot, $scope.board);
+			if(y){
+				$scope.score++;
+			}
+			clearSpot($scope.board);
+			createRandom($scope.board);
+		};
+	});
+
+	function check(spot, board){
+		var spotm = board.rows[row].spots[column];
+		var spotc = board.rows[6-row].spots[6-column];
+		if(spotm.content=="good"){
+			if(spot==spotm){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		if(spotm.content=="bad"){
+			if(spot==spotc){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}		
+	}
+
+	function clearSpot(board){
+		var spotx = board.rows[row].spots[column];
+		spotx.content = "empty";
+	}
+
+	function createRandom(board){
+		row = Math.floor(Math.random()*7);
+		column = Math.floor(Math.random()*7);
+		var x = Math.floor(Math.random()*5);
+		var spot = getSpot(board, row, column);
+		if(x%5==0){
+			spot.content="good";
+		}
+		else{
+			spot.content="bad";
+		}
+	}
+
+	function getSpot(board){
+		return board.rows[row].spots[column];
+	}
+
+
+	function createBoard() {
+	    var board = {};
+	    board.rows = [];
+	    
+	    for(var i = 0; i < 7; i++) {
+	        var row = {};
+	        row.spots = [];
+	        
+	        for(var j = 0; j < 7; j++) {
+	            var spot = {};
+	            spot.content="empty";
+	            row.spots.push(spot);
+	        }
+	        board.rows.push(row);
+	    }
+	    return board;
+	}
+
+})();
