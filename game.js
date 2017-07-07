cfbe2b9f9869ec99fa9aa1993d78ff6ba6f44554
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-07 22:33:47 +0530

    added time feature

--- game.js
+++ game.js
@@ -3,6 +3,7 @@
 	var app = angular.module('game', [ ]);
 	var row = 0;
 	var column = 0;
+	var dim = 7;
 
 	app.controller('GameController', function($scope){
 		$scope.board = createBoard();
@@ -13,15 +14,27 @@
 			createRandom($scope.board);
 			$scope.started = true;
 			$scope.loss = false;
+			$scope.times = false;
 			$scope.scoreboard = true;
 			$scope.score = 0;
+			clock = new Date().getTime();
 		};
 
 		$scope.go = function(spot){
 			if($scope.started){
 				var y=check(spot, $scope.board);
 				if(y){
-					$scope.score++;
+					clock1 = new Date().getTime();
+					if(clock1-clock<2000){
+						$scope.score+=(2000-clock1+clock)/1000;
+						$scope.score=Math.round($scope.score*1000)/1000;
+						clock = clock1;
+					}
+					else{
+						$scope.times = true;
+						$scope.scoreboard = false;
+						$scope.started = false;
+					}
 				}
 				else{
 					$scope.loss = true;
@@ -34,9 +47,10 @@
 		};
 	});
 
+
 	function check(spot, board){
-		var spotm = board.rows[row].spots[column];
-		var spotc = board.rows[6-row].spots[6-column];
+		var spotm = getSpot(board, row, column);
+		var spotc = getSpot(board, dim-1-row, dim-1-column);
 		if(spotm.content=="good"){
 			if(spot==spotm){
 				return true;
@@ -61,8 +75,8 @@
 	}
 
 	function createRandom(board){
-		row = Math.floor(Math.random()*7);
-		column = Math.floor(Math.random()*7);
+		row = Math.floor(Math.random()*dim);
+		column = Math.floor(Math.random()*dim);
 		var x = Math.floor(Math.random()*5);
 		var spot = getSpot(board, row, column);
 		if(x%5==0){
@@ -73,7 +87,7 @@
 		}
 	}
 
-	function getSpot(board){
+	function getSpot(board, row, column){
 		return board.rows[row].spots[column];
 	}
 
@@ -82,11 +96,11 @@
 	    var board = {};
 	    board.rows = [];
 	    
-	    for(var i = 0; i < 7; i++) {
+	    for(var i = 0; i < dim; i++) {
 	        var row = {};
 	        row.spots = [];
 	        
-	        for(var j = 0; j < 7; j++) {
+	        for(var j = 0; j < dim; j++) {
 	            var spot = {};
 	            spot.content="empty";
 	            row.spots.push(spot);
